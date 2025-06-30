d1a73b6bb359df2b270494996b3b2473dce1b289
Angela <212233529+LoopMint@users.noreply.github.com>
2025-06-30 17:21:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,3 @@
-st.title("Capstone Project: QS Ranking Analysis 2025")
-st.write(
-    "Data source: Kaggle (https://www.kaggle.com/datasets/darrylljk/worlds-best-universities-qs-rankings-2025)."
-)
-
 import streamlit as st
 import pandas as pd
 import tensorflow as tf
